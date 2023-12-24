44b46b7ee9f45759e139a62cb437018210b8fc88
Novojit Saha <n.saha@student.utwente.nl>
2023-12-24 18:36:04 +0100

    -Basic directory iterator in main.rs - Update .gitignore to ignore test directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let dirPath = "./test";
+
+    if let Ok(entries) = fs::read_dir(dirPath){
+        for entry in entries{
+            if let Ok(entry) = entry{
+                let entryPath = entry.path();
+                let entryName = entry.file_name();
+
+                if entryPath.is_file(){
+                    println!("File name: {:?}", entryName);
+                } else if entryPath.is_dir(){
+                    println!("Directory: {:?}", entryName );
+                }
+
+            }
+        }
+    } else {
+        eprintln!("Failed to read directory: {:?}", dirPath);
+    }
 }
+
+
